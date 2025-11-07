d4d114a3319127dc961c928749167c9c89e72bc1
Guillaume (Goliver) Golay <goliworks@gmail.com>
2025-11-07 23:36:17 +0100

    feat: display default webcam

--- lib/cam_area.dart
+++ lib/cam_area.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rust_cam_test/src/rust/api/simple.dart';
 import 'package:flutter_rust_cam_test/src/rust/api/camera.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated_common.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class CamArea extends StatefulWidget {
   const CamArea({super.key});
@@ -36,6 +37,30 @@ class _CamAreaState extends State<CamArea> {
     });
   }
 
+  RTCVideoRenderer? _renderer;
+  MediaStream? _stream;
+
+  void _openCamera() async {
+    // create and initialize renderer
+    _renderer ??= RTCVideoRenderer();
+    await _renderer!.initialize();
+
+    //
+    try {
+      _stream = await navigator.mediaDevices.getUserMedia({
+        'audio': false,
+        'video': true,
+      });
+    } catch (e) {
+      //if you get an error, please check the permissions in the project settings.
+      print(e.toString());
+    }
+
+    // set the MediaStream to the video renderer
+    _renderer!.srcObject = _stream;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -55,13 +80,13 @@ class _CamAreaState extends State<CamArea> {
           width = height * aspectRatio;
         }
 
-        Uint8List? image;
-
-        try {
-          image = getImage(file: "/tmp/test.jpg");
-        } catch (e) {
-          print(e);
-        }
+        // Uint8List? image;
+        //
+        // try {
+        //   image = getImage(file: "/tmp/test.jpg");
+        // } catch (e) {
+        //   print(e);
+        // }
 
         // limit to 1280x720
         width = width.clamp(0, maxWidth);
@@ -74,15 +99,19 @@ class _CamAreaState extends State<CamArea> {
                 width: width,
                 height: height,
                 color: Colors.blueGrey,
-                child: image != null
-                    ? Image.memory(image, fit: BoxFit.cover)
-                    : Center(child: Text("No image")),
+                child: SizedBox(
+                  // render the video renderer in the widget tree
+                  child: _renderer != null
+                      ? RTCVideoView(_renderer!)
+                      : Container(),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: dropdownList.isNotEmpty
-                    ? DropdownMenu(dropdownMenuEntries: dropdownList)
-                    : Text("Cameras not loaded"),
+                child: FilledButton(
+                  onPressed: _openCamera,
+                  child: const Text("Open Camera"),
+                ),
               ), // add some padding)
             ],
           ),
