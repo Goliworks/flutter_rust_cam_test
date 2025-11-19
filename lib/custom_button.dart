33b1f0b92703b482a7272682e6d630dd046b947d
Guillaume (Goliver) Golay <goliworks@gmail.com>
2025-11-19 18:43:12 +0100

    feat: hover effect

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -1,33 +1,54 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
+class CustomButton extends StatefulWidget {
   final String? imagePath;
   final String? text;
   final VoidCallback? onPressed;
-
   const CustomButton({super.key, this.imagePath, this.text, this.onPressed});
 
   @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  bool _hover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressed,
-      child: Container(
-        width: 150,
-        height: 120,
-        decoration: BoxDecoration(
-          color: Colors.grey[300], // gris clair
-          border: Border.all(
-            color: Colors.grey[500]!, // gris foncé
-            width: 1,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hover = true),
+      onExit: (_) => setState(() => _hover = false),
+      child: InkWell(
+        onTap: widget.onPressed,
+        child: Container(
+          width: 150,
+          height: 120,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            border: Border.all(
+              color: _hover ? Colors.orange : Colors.grey,
+              width: 1,
+            ),
+            boxShadow: _hover
+                ? [BoxShadow(color: Colors.orange, spreadRadius: 1)]
+                : [],
           ),
-        ),
-        child: Center(
-          child: imagePath != null && imagePath!.isNotEmpty
-              ? Image.asset(imagePath!, fit: BoxFit.cover)
-              : Text(
-                  text ?? 'No image',
-                  style: TextStyle(color: Colors.black54, fontSize: 18),
-                ),
+          child: ClipRRect(
+            child: Align(
+              alignment: Alignment.center,
+              child: widget.imagePath != null && widget.imagePath!.isNotEmpty
+                  ? Image.asset(
+                      widget.imagePath!,
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 120,
+                    )
+                  : Text(
+                      widget.text ?? 'No image',
+                      style: TextStyle(color: Colors.black54, fontSize: 18),
+                    ),
+            ),
+          ),
         ),
       ),
     );
